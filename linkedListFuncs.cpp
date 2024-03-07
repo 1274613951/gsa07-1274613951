742e897d3c7e905bb053efcc1e76937d62f9cc92
yiheng feng <yihengfeng@ucsb.edu>
2024-03-06 22:39:05 -0800

    4

--- linkedListFuncs.cpp
+++ linkedListFuncs.cpp
@@ -24,9 +24,6 @@ return head->data + recursiveSum(head->next);
 //return the largest value in the linked list using a recursive approach
 //you may assume the list has at least one element
 int recursiveLargestValue(Node* head) {
-    if(head == nullptr){
-        return 0;
-    }
     int large = recursiveLargestValue(head->next);
     if(head->data > large){
         return head->data;
